e74ea4e7259434c6250ed3389398efa98c341825
Petr Gregor <petr.gregor@email.cz>
2026-07-14 19:02:56 +0200

    Create .env file.

--- MovieDB/settings.py
+++ MovieDB/settings.py
@@ -9,24 +9,26 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bm*ms1__pe(xlaspccj^drg+iq(j1ig8eai8l2^09zttq@=4f$'
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY',
+                       default='django-insecure-)si+fpno3#)=7__vx-4%ni^&n1wvaz9bju1e+s8*i!e9qt!@f)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -68,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MovieDB.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
@@ -78,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -98,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -109,7 +108,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
---
+++ README.md
@@ -0,0 +1,42 @@
+# Django
+## Instalace 
+```bash
+pip install django
+pip freeze > requirements.txt
+django-admin startproject <nazev_projektu> .
+```
+
+## Struktura projektu
+- `MovieDB` - složka projektu (obsahuje informace o celém projektu)
+  - `__init__.py` - je zde jen proto, aby daná složka byla package
+  - `asgi.py` - nebudeme potřebovat
+  - `settings.py` - nastavení projektu
+  - `urls.py` - zde jsou nastavené url adresy
+  - `wsgi.py` - nebudeme potřebovat
+- `manage.py` - hlavní skript pro práci s projektem (souštění serveru, testů, migrací,...)
+
+## Spuštění serveru
+```bash
+python manage.py runserver
+```
+
+## .gitignore
+Správný projekt má nastavení pro ingorované soubory v git repozitáři v souboru `.gitignore`.
+
+## .env
+Slouží k ukládání citlivých informací (SECRET_KEY, hesla, přístupové údaje,...).
+
+### Instalace
+```bash
+pip install python-dotenv
+pip freeze > requirements.txt
+```
+
+Vytvoříme soubour s názvem `.env` v kořenovém adresáři projektu.
+
+Do souboru `settings.py` vložíme:
+```python
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-)si+fpno3#)=7__vx-4%ni^&n1wvaz9bju1e+s8*i!e9qt!@f)')
+```
