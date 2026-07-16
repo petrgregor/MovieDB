014db55a128a13af02cc42df668c9b2de84de1c5
Petr Gregor <petr.gregor@email.cz>
2026-07-16 19:34:11 +0200

    Templates.

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    movie = "Forrest Gump"
+    released = 1994
+    comments = ["Skvělý film.",
+                "Velmi se mi to líbilo.",
+                "Perfektní.",
+                "Šlo to."]
+    context = {'movie': movie, 'released': released, 'comments': comments}
+    return render(request, 'viewer/home.html', context)
